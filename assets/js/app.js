edaaebcd2ec63a7ca6565acaf275a8cef75ff73a
Lateef Adeyekun <lateef.adeyekun@gmail.com>
2024-12-11 13:18:02 -0600

    added 3 cols

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,10 @@
 
 import {select, listen} from "./util.js";
 
+mapboxgl.accessToken = 'pk.eyJ1IjoiZWhyZW4tc3RyaWZsaW5nIiwiYSI6ImNscTN3dmZoYjAxMG4ydm14ZnNjaWtqOW0ifQ.PtNGzOxZJvB9XIJGME7k3Q';
+const map = new mapboxgl.Map({
+  container: 'map-container',
+  style: 'mapbox://styles/mapbox/streets-v12',
+  center: [-74.5, 40],
+  zoom: 3
+});
