8ec714ec636b86a8d32937429ffbabdf7fa4b5a6
Lateef Adeyekun <lateef.adeyekun@gmail.com>
2024-12-11 23:21:48 -0600

    footer added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,13 @@
 'use strict';
 
-import {API_KEY, select, listen, create, sleep} from "./util.js";
+import {select, listen, create, sleep} from "./util.js";
 
+const API_KEY = 'pk.eyJ1IjoiZWhyZW4tc3RyaWZsaW5nIiwiYSI6ImNscTN3dmZoYjAxMG4ydm14ZnNjaWtqOW0ifQ.PtNGzOxZJvB9XIJGME7k3Q';
 const mapContainer = select('.map-container');
+const locationBtn = select('.location');
+const options = { enableHighAccuracy: true };
 
-mapboxgl.accessToken = API_KEY;
+//mapboxgl.accessToken = API_KEY;
 
 const geoJSON = {
   type: 'FeatureCollection',
@@ -24,22 +27,53 @@ const geoJSON = {
 };
 
 const map = new mapboxgl.Map({
-  container: 'map-container',
+  accessToken: API_KEY,
+  container: mapContainer,
   style: 'mapbox://styles/mapbox/streets-v12',
   center: [-74.5, 40],
   zoom: 3
 });
-
+/*
 function setLocationMarker() {
   for (const feature of geoJSON.features) {
     const el = create('div');
     el.className = 'marker';
   
-    new mapboxgl.Marker(el).setLngLat(feature.geometry.coordinates).addTo(map);
+    //new mapboxgl.Marker(el).setLngLat(feature.geometry.coordinates).addTo(map);
+    new mapboxgl.Marker(el).setLngLat(getDe).addTo(map);
   }
 }
+  */
 
-setLocationMarker();
+function setLocationMarker(position) {
+  let { latitude, longitude } = position.coords;
+
+  const el = create('div');
+  el.className = 'marker';
+
+  new mapboxgl.Marker(el).setLngLat([longitude, latitude]).addTo(map);
+}
+
+function errorHandler() {
+
+}
+
+listen('load', window, () => {
+  //setLocationMarker();
+});
+
+listen('click', locationBtn, () => {
+  if ('geolocation' in navigator) {
+    const el = create('div');
+    el.className = 'marker';
+    
+    navigator.geolocation.getCurrentPosition(
+      setLocationMarker, errorHandler, options
+    );
+  } else {
+   // display error that geolocation is not supported
+  }
+});
 
 
 
--- assets/js/util.js
+++ assets/js/util.js
@@ -1,7 +1,5 @@
 'use strict';
 
-export const API_KEY = 'pk.eyJ1IjoiZWhyZW4tc3RyaWZsaW5nIiwiYSI6ImNscTN3dmZoYjAxMG4ydm14ZnNjaWtqOW0ifQ.PtNGzOxZJvB9XIJGME7k3Q';
-
 export function select(selector, scope = document) {
   return scope.querySelector(selector);
 }
